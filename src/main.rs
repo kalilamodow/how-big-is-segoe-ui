4ed03ceee3208a30dd7fc37ca6fb3c47d9aeed9e
Kalilamodow <kalilamodow@gmail.com>
2026-09-10 11:35:52 -0500

    how big is it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::Result;
+fn main() -> Result<()> {
+    let file = File::open("C:\\Windows\\Fonts\\segoeui.ttf")?;
+    let metadata = file.metadata()?;
+    let file_size = metadata.len();
+    println!("{} bytes", file_size);
+    Ok(())
 }
